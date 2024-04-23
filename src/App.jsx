1e8dc65a022e5b57f7b6bdbc3dcc8dd2780893e2
Merylcheng <163097111+Merylcheng@users.noreply.github.com>
2024-04-23 17:18:36 +0800

    api calories works. console.log mealData

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-  async function logMovies() {
-    const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?minCalories=50&maxCalories=500&number=3&includeIngredients=tomato,cheese&addRecipeInstructions=true&apiKey=5fd761dd54a74faf8f5d37f3d4570012");
-    const movies = await response.json();
-    console.log(movies);
-  }
+import { useState } from "react";
+import MealList from './MealList'
 
 function App() {
+  
+  const [mealData, setMealData] = useState('')
+  const [calories, setCalories] = useState('')
 
-  logMovies()
+  function handleChange(event) {
+    setCalories(event.target.value)
 
-
-
+  }  
+  
+  async function getMealData() {
+    const response = await fetch("https://api.spoonacular.com/recipes/complexSearch?minCalories=50&maxCalories=2000&number=10&apiKey=5fd761dd54a74faf8f5d37f3d4570012");
+    const mealData = await response.json();
+    setMealData(mealData);
+    console.log(mealData)
+  }
 
     return (
+      <div className = 'App'>
+      <h1>Calories Max</h1>
+      <section className = 'controls'>
+        <input
+        type='number'
+        placeholder = 'Calories (eg. 500)'
+        onChange={handleChange} />
+      </section>
+      <button onClick={getMealData}>Get Meal </button>
+      </div>
 
-
-  
- <h1>test</h1>
 
     )
   
---
+++ src/MealList.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function MealList() {
+
+
+
+
+    return (
+        <div>
+            MealList
+        </div>
+    )
+}
